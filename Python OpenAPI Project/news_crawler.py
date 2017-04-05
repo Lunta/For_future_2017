70bea4bf25e8e504159fe1566e50be8d2aa9bd96
Sang-gi Lee <tkdqlsek2@gmail.com>
2017-04-05 11:14:03 +0900

    python project updated.
    crawler updated

--- Python OpenAPI Project/news_crawler.py
+++ Python OpenAPI Project/news_crawler.py
@@ -1,33 +1,62 @@
-"""네이버 뉴스 기사 웹 크롤러 모듈"""
+""" 동아일보 특정 키워드를 포함하는, 특정 날짜 이전 기사 내용 크롤러(정확도순 검색)
+    python [모듈 이름] [키워드] [가져올 페이지 숫자] [결과 파일명]
+    한 페이지에 기사 15개
+"""
 
+import sys
 from bs4 import BeautifulSoup
 import urllib.request
+from urllib.parse import quote
+import text_cleaner
 
-# 출력 파일 명
-OUTPUT_FILE_NAME = 'output.txt'
-# 긁어 올 URL
-URL = 'http://news.naver.com/main/read.nhn?mode=LSD&mid=shm&sid1=103&oid=055' \
-      '&aid=0000445667'
-
-
-# 크롤링 함수
-def get_text(URL):
-    source_code_from_URL = urllib.request.urlopen(URL)
-    soup = BeautifulSoup(source_code_from_URL, 'lxml', from_encoding='utf-8')
-    text = ''
-    for item in soup.find_all('div', id='articleBodyContents'):
-        text = text + str(item.find_all(text=True))
-    return text
+TARGET_URL_BEFORE_PAGE_NUM = "http://news.donga.com/search?p="
+TARGET_URL_BEFORE_KEWORD = '&query='
+TARGET_URL_REST = '&check_news=1&more=1&sorting=3&search_date=1&v1=&v2=&range=3'
 
 
-# 메인 함수
-def main():
-    open_output_file = open(OUTPUT_FILE_NAME, 'w')
-    result_text = get_text(URL)
-    print(result_text)
-    open_output_file.write(str(result_text))
-    open_output_file.close()
+# 기사 검색 페이지에서 기사 제목에 링크된 기사 본문 주소 받아오기
+def get_link_from_news_title(page_num, URL, output_file):
+    for i in range(page_num):
+        current_page_num = 1 + i*15
+        position = URL.index('=')
+        URL_with_page_num = URL[: position+1] + str(current_page_num) \
+                            + URL[position+1 :]
+        source_code_from_URL = urllib.request.urlopen(URL_with_page_num)
+        soup = BeautifulSoup(source_code_from_URL, 'lxml',
+                             from_encoding='utf-8')
+        for title in soup.find_all('p', 'tit'):
+            title_link = title.select('a')
+            article_URL = title_link[0]['href']
+            get_text(article_URL, output_file)
+
+
+# 기사 본문 내용 긁어오기 (위 함수 내부에서 기사 본문 주소 받아 사용되는 함수)
+def get_text(URL, output_file):
+    source_code_from_url = urllib.request.urlopen(URL)
+    soup = BeautifulSoup(source_code_from_url, 'lxml', from_encoding='utf-8')
+    content_of_article = soup.select('div.article_txt')
+    for item in content_of_article:
+        string_item = str(item.find_all(text=True))
+        output_file.write(string_item)
+
+
+# 메인함수
+def main(argv):
+    if len(argv) != 4:
+        print("python [모듈이름] [키워드] [가져올 페이지 숫자] [결과 파일명]")
+        return
+    keyword = argv[1]
+    page_num = int(argv[2])
+    output_file_name = argv[3]
+    target_URL = TARGET_URL_BEFORE_PAGE_NUM + TARGET_URL_BEFORE_KEWORD \
+                 + quote(keyword) + TARGET_URL_REST
+    output_file = open(output_file_name, 'w')
+    get_link_from_news_title(page_num, target_URL, output_file)
+    output_file.close()
+    text_cleaner.INPUT_FILE_NAME = output_file_name
+    text_cleaner.text_cleaning()
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
+
