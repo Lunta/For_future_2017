87b610d9bc2887ab71f15dfcde3a967d007d1608
Sang-gi Lee <tkdqlsek2@gmail.com>
2017-05-17 09:46:20 +0900

    Updated query part

--- Python OpenAPI Project/query_data.py
+++ Python OpenAPI Project/query_data.py
@@ -1,16 +1,34 @@
 import urllib.parse
 import urllib.request
 
-url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty'
-queryParams = '?&' + urllib.parse.urlencode({ urllib.parse.quote_plus('ServiceKey') : 'LkXZ0KCCJ4B52toUWxsA5xcobsnrNLOB55MHxbJbVBj1WUZ6P2BkN1Qka%2BytNClkxQbFC3mvvhxkI163p%2BTHjg%3D%3D',
-                                             urllib.parse.quote_plus('stationName'): '종로구'.encode('utf-8'),
-                                             urllib.parse.quote_plus('dataTerm'): 'month',
-                                             urllib.parse.quote_plus('pageNo'): '1',
-                                             urllib.parse.quote_plus('numOfRows'): '10'})
+str = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?' \
+      'stationName='+urllib.parse.quote('정왕동')+'&' \
+      'dataTerm=month&' \
+      'pageNo=1&' \
+      'numOfRows=24&' \
+      'ServiceKey=LkXZ0KCCJ4B52toUWxsA5xcobsnrNLOB55MHxbJbVBj1WUZ6P2BkN1Qka%2BytNClkxQbFC3mvvhxkI163p%2BTHjg%3D%3D&' \
+      'ver=1.3'
+# 등급	    좋음	보통	나쁨	매우나쁨
+# Grade 값	1	    2	    3	    4
+# 항목	    SO2	    CO	        O3	    NO2	         PM10	PM2.5
+# 이름      아황산  일산화탄소  오존    이산화질소   미세    초미세
+# 단위	    ppm	    ppm	        ppm	    ppm	         ㎍/㎥	㎍/㎥
 
-
-request = urllib.request.Request(url + queryParams)
+request = urllib.request.Request(str)
 request.get_method = lambda: 'GET'
-response_body = urllib.request.urlopen(request).read()
+response_body = urllib.request.urlopen(request).read().decode('utf-8')
 print (response_body)
 
+#import cairosvg
+#import rsvg
+#cairosvg.surface.PNGSurface=
+#img = cairo.ImageSurface(cairo.FORMAT_ARGB32, 640,480)
+#ctx = cairo.Context(img)
+#
+### handle = rsvg.Handle(<svg filename>)
+## or, for in memory SVG data:
+#handle= rsvg.Handle(None, 'Administrative_divisions_map_of_South_Korea.svg')
+#
+#handle.render_cairo(ctx)
+#
+#img.write_to_png("svg.png")
